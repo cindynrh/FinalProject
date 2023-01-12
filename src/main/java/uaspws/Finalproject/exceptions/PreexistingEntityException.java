1a141e851eb12a23f9d7051c62ffe21613e082d6
cindynrh <cindynurhabibah@gmail.com>
2023-01-12 09:50:50 +0700

    Uas Pws index, controller

---
+++ src/main/java/uaspws/Finalproject/exceptions/PreexistingEntityException.java
@@ -0,0 +1,10 @@
+package uaspws.Finalproject.exceptions;
+
+public class PreexistingEntityException extends Exception {
+    public PreexistingEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public PreexistingEntityException(String message) {
+        super(message);
+    }
+}
---
+++ src/test/java/uaspws/Finalproject/FinalprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package uaspws.Finalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
